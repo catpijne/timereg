fc30584487741acfcec68f2f67ab99ba670b018a
Christiaan Pijnenburg <christiaan.pijnenburg@centric.eu>
2018-07-25 16:45:30 +0200

    nu met Commend

--- Test v0.1/test_v1.js
+++ Test v0.1/test_v1.js
@@ -1,10 +1,11 @@
 document.body.style.background = "rgb(255, 255, 255)"; 
 let Default_KM_traveld=0;
+let Default_KM_commend=""
 console.log(Default_KM_traveld);
 
 
-Default_KM_traveld = localStorage.getItem("Declaratie_kilometers");
-
+    Default_KM_commend = localStorage.getItem("Declaratie_kilometers").Default_KM_commend;
+    Default_KM_traveld = localStorage.getItem("Declaratie_kilometers").Default_KM_traveld;
 
 
 
@@ -17,6 +18,11 @@ function addUserInterface(){
   node.setAttribute("value", "KM");
   node.setAttribute("id", "KM");
   UI_interface.appendChild(node);
+  node = document.createElement("INPUT");
+  node.setAttribute("type", "text");
+  node.setAttribute("value", "Commend");
+  node.setAttribute("id", "Commend");
+  UI_interface.appendChild(node);
   node = document.createElement("BUTTON");
   let textnode = document.createTextNode("New default");
   node.appendChild(textnode);
@@ -26,8 +32,11 @@ function addUserInterface(){
 
 
 function setDeaultKM(){
-  
+  Default_KM_commend=document.getElementById("Commend").value;
   Default_KM_traveld=document.getElementById("KM").value;
+  let Default_KM ={ Default_KM_commend,
+  Default_KM_traveld}
+  console.log(Default_KM);
   localStorage.setItem("Declaratie_kilometers", Default_KM_traveld);
   setKMValueInSheet();
 }
@@ -36,9 +45,10 @@ function setKMValueInSheet(){
   let maxLength =document.getElementsByClassName("PortletText2").length;
   console.log(maxLength);
   let i = maxLength-8;
-  for( i; i < maxLength; i++){
+  for( i; i < maxLength-3; i++){
   	document.getElementsByClassName("PortletText2")[i].lastChild.value = Default_KM_traveld;
-	}
+    }
+    document.getElementsByClassName("PortletText2")[maxLength-2].lastChild.value =Default_KM_commend;
    
 }
 setKMValueInSheet();
