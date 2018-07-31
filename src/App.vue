7379241dae93742a834f0d234352492d36934ca6
Maarten Göertz <maarten.goertz@centric.eu>
2018-07-31 10:51:16 +0200

    Made vue app work in timereg

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <table class="UI_checkbox_table">
+      <tr id="UI_checkbox_header">
+        <th>location</th>
+        <th>KM</th>
+        <th>Commentaar</th>
+        <th>maandag</th>
+        <th>dinsdag</th>
+        <th>woensdag</th>
+        <th>donderdag</th>
+        <th>vrijdag</th>
+      </tr>
+      <tr id="UI_locationInfo">
+        <td class="locationName"><input id="locationName" type="text"></td>
+        <td class="KM"><input id="KM" type="text"></td>
+        <td class="Commend"><input id="Commend" type="text"></td>
+        <td class="Maandag"><input id="Maandag" type="checkbox"></td>
+        <td class="Dinsdag"><input id="Dinsdag" type="checkbox"></td>
+        <td class="Woensdag"><input id="Woensdag" type="checkbox"></td>
+        <td class="Donderdag"><input id="Donderdag" type="checkbox"></td>
+        <td class="Vrijdag"><input id="Vrijdag" type="checkbox"></td>
+      </tr>
+    </table>
+    <button id="ButtonDefault">New default</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
