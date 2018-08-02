227adc3c2e05494820f17f05093bc955c3cdb9cb
Maarten Göertz <maarten.goertz@centric.eu>
2018-08-02 14:07:14 +0200

    fixed semistandard autofixable issues

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,14 +1,12 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+'use strict';
+const path = require('path');
+const utils = require('./utils');
+const config = require('../config');
+const vueLoaderConfig = require('./vue-loader.conf');
 
 function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+  return path.join(__dirname, '..', dir);
 }
-
-
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
@@ -26,7 +24,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -79,4 +77,4 @@ module.exports = {
     tls: 'empty',
     child_process: 'empty'
   }
-}
+};
